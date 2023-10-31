cc7a9367e05d77987f823ef4e0619a025dabec9a
Rizwan Ahmad <113388795+Riz1981@users.noreply.github.com>
2023-10-31 11:43:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+//print Quots and its Author name
+let Quots = "Determination brings the right perspectives...";
+let authorName = "Muhammad ali jinnah";
+let message = `${authorName} said,"${Quots}"`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,5 @@
+//print Quots and its Author name
+let Quots : string = "Determination brings the right perspectives...";
+let authorName:string ="Muhammad ali jinnah";
+let message: string = `${authorName} said,"${Quots}"`;
+console.log(message);
